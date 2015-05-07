3d26dd7898f7ca0ec53e30795e991aaf4855f9a9
divneet <divneetkochar@gmail.com>
2015-05-07 16:07:47 +0530

    asteroid created , movement towards enemies completed , explosion created

---
+++ Assets/scripts/DestroyByContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject Explosion;
+	public GameObject PlayerExplosion;
+	void OnTriggerEnter (Collider other )
+	{
+
+			if (other.tag == "Boundary")
+			{
+				return;
+			}
+		//Debug.Log (other.name);
+		Instantiate (Explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+						Instantiate (PlayerExplosion, transform.position, transform.rotation);
+				}
+		Destroy(other.gameObject);
+		Destroy (gameObject);
+	}
+ }
